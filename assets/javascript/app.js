659cbb8be8fffe4c0dbab1836c334c43853773cb
isaac <simstarr18@yahoo.com>
2018-05-21 17:56:57 -0700

    initial site files

---
+++ assets/javascript/app.js
@@ -0,0 +1,166 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var oneMinutesThirty = 90 * 1,
+        display = document.querySelector('#time');
+    startTimer(oneMinutesThirty, display);
+};
+(function() {
+  function buildQuiz() {
+
+    const output = [];
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+      const answers = [];
+
+      for (letter in currentQuestion.answers) {
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if (userAnswer === currentQuestion.correctAnswer) {
+
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Sample question 1?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 2?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 3?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 4?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 5?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 6?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 7?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+    {
+      question: "Sample question 8?",
+      answers: {
+        a: "a",
+        b: "b",
+        c: "d",
+        d: "c",
+      },
+      correctAnswer: "?"
+    },
+  ];
+
+  buildQuiz();
+
+  submitButton.addEventListener("click", showResults);
+})();
